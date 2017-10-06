83675c53812292a05230d15cc64ba377be925179
GeorgiMoskov <moskov120@abv.bg>
2017-10-06 22:44:20 +0300

    Added Solution for Problem 5

---
+++ 1 Tech Module/Programming Fundamentals/1. C# INTRO AND BASIC SYNTAX/Exercises/T5_CharacterStats/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5_CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var curHp = int.Parse(Console.ReadLine());
+            var maxHp = int.Parse(Console.ReadLine());
+            var curEng = int.Parse(Console.ReadLine());
+            var maxEng = int.Parse(Console.ReadLine());
+
+            
+            var dotsHp = new String('.', maxHp - curHp);
+            var linesHp = new String('|', curHp);
+
+            var dotsEng = new String('.', maxEng - curEng);
+            var linesEng = new String('|', curEng);
+
+            Console.WriteLine("Name: {0}",name);
+            Console.WriteLine("Health: |{0}{1}|",linesHp,dotsHp);
+            Console.WriteLine("Energy: |{0}{1}|", linesEng, dotsEng);
+
+        }   
+    }
+}
