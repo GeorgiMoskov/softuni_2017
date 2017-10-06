08e54f0b41c74ae200ea9171475649af1786bc10
GeorgiMoskov <moskov120@abv.bg>
2017-10-06 22:29:35 +0300

    Added PROBLEMS.docx

---
+++ 1 Tech Module/Programming Fundamentals/1. C# INTRO AND BASIC SYNTAX/Exercises/T2_RectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var sum = a * b;
+            Console.WriteLine("{0:F2}",sum);
+        }
+    }
+}
---
+++ 1 Tech Module/Programming Fundamentals/1. C# INTRO AND BASIC SYNTAX/Exercises/T4_BeverageLabels/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4_BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energy = int.Parse(Console.ReadLine());
+            var sugar = int.Parse(Console.ReadLine());
+
+            var allEnergy = (volume * (double)energy) / 100;
+            var allSugar = (volume * (double)sugar)/100;
+
+            Console.WriteLine("{0}ml {1}:",volume,name);
+            Console.WriteLine("{0}kcal, {1}g sugars",allEnergy,allSugar);
+
+
+        }
+    }
+}
