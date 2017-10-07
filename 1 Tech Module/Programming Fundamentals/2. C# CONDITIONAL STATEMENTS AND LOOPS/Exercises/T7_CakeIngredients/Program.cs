8243b6ad3810e16478c7d9356c1b2b1283dd85e5
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 15:46:03 +0300

    T7 ex Lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T7_CakeIngredients/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T7_CakeIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "";
+            var br = 0;
+            while (true)
+            {
+                str = Console.ReadLine();
+
+                if(str == "Bake!")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Adding ingredient {0}.", str);
+                    br++;
+                }
+
+            }
+
+            Console.WriteLine("Preparing cake with {0} ingredients.",br);
+        }
+    }
+}
