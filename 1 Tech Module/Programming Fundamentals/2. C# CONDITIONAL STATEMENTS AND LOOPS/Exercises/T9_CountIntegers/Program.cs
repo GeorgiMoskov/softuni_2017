0665242c1d0af160d0f355fc2bbb4cd038bf5fe4
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 16:32:20 +0300

    t9 ex lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T9_CountIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9_CountIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var br = 0;
+
+            while (true)
+            {
+                try
+                {
+                    var intNum = int.Parse(Console.ReadLine());
+                    br++;
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(br);
+
+        }
+    }
+}
