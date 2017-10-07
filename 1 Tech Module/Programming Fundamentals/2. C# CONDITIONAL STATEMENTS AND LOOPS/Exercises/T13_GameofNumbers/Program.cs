327d8a46db0f8840fadf2ed928cb7c946852d318
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 19:05:27 +0300

    T13 ex lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T13_GameofNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T13_GameofNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var magicNum = int.Parse(Console.ReadLine());
+            var combCount = 0;
+            var isComb = false;
+            var lasti = 0;
+            var lasty = 0;
+
+            for(int i = Math.Min(n, m); i <= Math.Max(n, m); i++)
+            {
+                for (int y = Math.Min(n, m); y <= Math.Max(n, m); y++)
+                {
+                    combCount++;
+                    if(i+y == magicNum)
+                    {
+                        isComb = true;
+                        lasti = i;
+                        lasty = y;
+
+                        
+                    }
+                }
+            }
+
+            if (isComb == true)
+            {
+                Console.WriteLine($"Number found! {lasti} + {lasty} = {magicNum}");
+            }
+            else
+            {
+                Console.WriteLine($"{combCount} combinations - neither equals {magicNum}");
+            }
+           
+
+
+
+        }
+    }
+}
