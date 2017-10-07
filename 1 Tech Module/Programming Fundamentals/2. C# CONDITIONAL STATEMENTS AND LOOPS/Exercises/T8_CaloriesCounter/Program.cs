4f32fce5526a4037cf19799802bff18afdef5bd4
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 15:55:57 +0300

    T8 ex Lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T8_CaloriesCounter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8_CaloriesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productsCount = int.Parse(Console.ReadLine());
+            var calories = 0;
+
+            for(var i = 0; i < productsCount; i++)
+            {
+                var product = Console.ReadLine();
+                product = product.ToLower();
+
+                switch (product)
+                {
+                    case "cheese":
+                        calories += 500;
+                        break;
+                    case "tomato sauce":
+                        calories += 150;
+                        break;
+                    case "salami":
+                        calories += 600;
+                        break;
+                    case "pepper":
+                        calories += 50;
+                        break;
+                    default:
+                        break;
+
+                }
+                
+            }
+
+            Console.WriteLine($"Total calories: {calories}");
+        }
+    }
+}
