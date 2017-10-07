49e6c0aa9dedc94a300ce8331659be5f5a0da386
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 12:48:46 +0300

    Added Solution for Task 1 from Exercises in Lecture 2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T1_ChooseDrink/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1_ChooseDrink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var profession = Console.ReadLine();
+            var drink = "Tea";
+
+            if(profession == "Athlete")
+            {
+                drink = "Water";
+            }
+            else if(profession == "Businessman" || profession == "Businesswoman")
+            {
+                drink = "Coffee";
+            }
+            else if(profession == "SoftUni Student")
+            {
+                drink = "Beer";
+            }
+
+            Console.WriteLine(drink);
+        }
+    }
+}
