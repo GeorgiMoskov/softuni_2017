7c5311548d34d9a26e5fed30d38dc1a76625bf91
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 15:38:42 +0300

    T6 solution Ex lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T6_IntervalofNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6_IntervalofNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+
+            for(int i = Math.Min(a, b); i <= Math.Max(a, b); i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
