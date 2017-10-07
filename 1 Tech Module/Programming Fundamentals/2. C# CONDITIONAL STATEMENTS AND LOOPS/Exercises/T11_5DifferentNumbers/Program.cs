286778d9b389cdcc6c2e4f75d978979ffcba815e
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 17:16:02 +0300

    T11 ex lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T11_5DifferentNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T11_5DifferentNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+
+            if (b - a < 5)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            for (var i1 = a; i1 <= b - 4; i1++)
+            {
+                for (var i2 = i1+1; i2 <= b - 3; i2++)
+                {
+                    for (var i3 = i2+1; i3 <= b - 2; i3++)
+                    {
+                        for (var i4 = i3+1; i4 <= b - 1; i4++)
+                        {
+                            for (var i5 = i4+1; i5 <= b; i5++)
+                            {
+                                Console.WriteLine("{0} {1} {2} {3} {4}", i1, i2, i3, i4, i5);
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
