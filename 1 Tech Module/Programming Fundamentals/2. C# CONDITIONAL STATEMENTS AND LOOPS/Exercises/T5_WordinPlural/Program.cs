a29724b5193dd2f7db02ca11c5ec1691db276bce
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 15:30:09 +0300

    T5 in Ex. from Lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T5_WordinPlural/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5_WordinPlural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+
+            if(str[str.Length-1] == 'y')
+            {
+                str = str.Remove(str.Length - 1);
+                str = str + "ies";
+            }
+            else if( str.EndsWith("o") || str.EndsWith("ch") || str.EndsWith("s") || str.EndsWith("sh") || str.EndsWith("x") || str.EndsWith("z"))
+            {
+                str = str + "es";
+            }
+            else
+            {
+                str = str + "s";
+            }
+
+            Console.WriteLine(str);
+            
+        }
+    }
+}
