a3956cb733f7295b20cb2f07d4c7bed487c8d453
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 16:44:02 +0300

    T10 ex lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T10_TriangleofNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T10_TriangleofNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <=n; i++)
+            {
+                for(int y = 0; y < i; y++)
+                {   
+                    
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
