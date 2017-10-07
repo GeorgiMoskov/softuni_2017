364f9d13d8d33f9286f6a3701f96e9936ab82f04
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 12:58:18 +0300

    Added solution for Problem 2 from Exercises in Lecture 2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T2_ChooseDrink_V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2_ChooseDrink_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var profession = Console.ReadLine();
+            var quantities = int.Parse(Console.ReadLine());
+            var drink = "Tea";
+            var price = 1.20;
+
+            if (profession == "Athlete")
+            {
+                drink = "Water";
+                price = 0.70;
+            }
+            else if (profession == "Businessman" || profession == "Businesswoman")
+            {
+                drink = "Coffee";
+                price = 1.00;
+            }
+            else if (profession == "SoftUni Student")
+            {
+                drink = "Beer";
+                price = 1.70;
+            }
+
+            price = quantities * price;
+            Console.WriteLine("The {0} has to pay {1:F2}.",profession,price);
+            
+        }
+    }
+}
