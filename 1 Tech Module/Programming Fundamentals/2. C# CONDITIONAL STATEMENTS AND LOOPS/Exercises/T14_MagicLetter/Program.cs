022732e3c1cd0ffad6f837f5b04177d9e19f3bf6
GeorgiMoskov <moskov120@abv.bg>
2017-10-07 19:48:51 +0300

    T14 ex lec2

---
+++ 1 Tech Module/Programming Fundamentals/2. C# CONDITIONAL STATEMENTS AND LOOPS/Exercises/T14_MagicLetter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T14_MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLetter = char.Parse(Console.ReadLine());
+            var secondLetter = char.Parse(Console.ReadLine());
+            var badLetter = char.Parse(Console.ReadLine());
+
+            var startCount = Math.Min((int)firstLetter, (int)secondLetter);
+            var endCount = Math.Max((int)firstLetter, (int)secondLetter);
+            var str = "";
+
+
+            for(var i = startCount; i <= endCount; i++)
+            {
+                for (var y = startCount; y <= endCount; y++)
+                {
+                    for (var z = startCount; z <= endCount; z++)
+                    {
+                        str = ((char)i).ToString() + (char)y + (char)z;
+
+                        if (str.Contains(badLetter.ToString()) == false)
+                        {
+                            Console.Write(str+" ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
