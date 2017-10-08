6ad06606fbfff2608f6bed7dac40e48b8a84c540
GeorgiMoskov <moskov120@abv.bg>
2017-10-08 11:42:10 +0300

    T10 ex lec3

---
+++ 1 Tech Module/Programming Fundamentals/3. DATA TYPES AND VARIABLES/Exercises/T10_CenturiesToNanoseconds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace T10_CenturiesToNanoseconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte centuries = byte.Parse(Console.ReadLine());
+            uint years = centuries * (uint)100;
+            uint days =(uint)Math.Floor(years * 365.2422);
+            //Console.WriteLine(days);
+            //Console.WriteLine(years * 365.2422);
+            //Console.WriteLine(Math.Round(years * 365.2422));
+            
+            ulong hours = days * 24;
+            BigInteger minutes = hours * 60;
+            BigInteger seconds = minutes * 60;
+            BigInteger milliseconds = seconds * 1000;
+            BigInteger microseconds = milliseconds * 1000;
+            BigInteger nanoseconds = microseconds * 1000;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+        }
+    }
+}
