3ae0217dce5b540df8c8a27eee269e37a6dea765
GeorgiMoskov <moskov120@abv.bg>
2017-10-08 13:59:20 +0300

    T16 ex lec3

---
+++ 1 Tech Module/Programming Fundamentals/3. DATA TYPES AND VARIABLES/Exercises/T16_ComparingFloats/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T16_ComparingFloats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double epsilon = 0.000001;
+
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            if(Math.Max(num1,num2) - Math.Min(num1,num2) < epsilon)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
