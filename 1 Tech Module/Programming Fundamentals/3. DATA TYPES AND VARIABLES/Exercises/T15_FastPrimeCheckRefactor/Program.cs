67ffd2f5144d2148f8f0280e96f9cbda16029b08
GeorgiMoskov <moskov120@abv.bg>
2017-10-08 13:53:26 +0300

    T16 ex lec3

---
+++ 1 Tech Module/Programming Fundamentals/3. DATA TYPES AND VARIABLES/Exercises/T15_FastPrimeCheckRefactor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T15_FastPrimeCheckRefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= num; i++)
+            {
+                bool isPrime = true;
+
+                for (int y = 2; y <=Math.Sqrt(i); y++)
+                {
+                    if (i % y == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isPrime}");
+            }
+
+
+
+        }
+    }
+}
