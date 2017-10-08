fdec850ab32dfe1dc8ff12ba2022cd5d21007c5a
GeorgiMoskov <moskov120@abv.bg>
2017-10-08 14:02:59 +0300

    T17 ex lec3

---
+++ 1 Tech Module/Programming Fundamentals/3. DATA TYPES AND VARIABLES/Exercises/T17_PrintPartOfTheAscii/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T17_PrintPartOfTheAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch1Code = int.Parse(Console.ReadLine());
+            int ch2Code = int.Parse(Console.ReadLine());
+
+            for(int i = ch1Code; i <= ch2Code; i++)
+            {
+                Console.Write("{0} ", (char)i);
+            }
+        }
+    }
+}
