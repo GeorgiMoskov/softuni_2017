a939f856da57ba8fa9642866a4cfce52569725a3
GeorgiMoskov <moskov120@abv.bg>
2017-10-08 17:24:56 +0300

    All solutions for exercises from lecture 3

---
+++ 1 Tech Module/Programming Fundamentals/3. DATA TYPES AND VARIABLES/Exercises/T19_ TheaThePhotographer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace T19__TheaThePhotographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long allPics = long.Parse(Console.ReadLine());
+            long secondsToFilterOnePic = long.Parse(Console.ReadLine());
+            long filterPercentage = long.Parse(Console.ReadLine());
+            long secondsToUpload = long.Parse(Console.ReadLine());
+
+
+            long secondsTakenToFilter = allPics * secondsToFilterOnePic;
+            long secondsTakenToUpload = ((long)Math.Ceiling((allPics / 100.0) * filterPercentage))*secondsToUpload;
+            long takenSecondsForEveryThing = secondsTakenToFilter + secondsTakenToUpload;
+            long days = 0;
+            long hours = 0;
+            long minutes = 0;
+            long seconds = 0;
+
+            seconds = takenSecondsForEveryThing % 60;
+            long leftInSeconds = takenSecondsForEveryThing - seconds;
+
+            minutes = (leftInSeconds / 60) % 60;
+            long leftInMinutes = (leftInSeconds / 60) - minutes;
+
+            hours = (leftInMinutes / 60) % 24;
+            long leftInHours = (leftInMinutes / 60) - hours;
+
+            days = leftInHours / 24;
+
+
+
+
+            Console.WriteLine($"{days}:{hours:D2}:{minutes:D2}:{seconds:D2}");
+
+        }
+    }
+}
