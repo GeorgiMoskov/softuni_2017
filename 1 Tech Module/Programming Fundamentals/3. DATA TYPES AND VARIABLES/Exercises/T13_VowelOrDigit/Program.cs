029223efe141f4c6a3d5e946078ffc66f749e82b
GeorgiMoskov <moskov120@abv.bg>
2017-10-08 13:13:05 +0300

    T13 and T14 ex lec3

---
+++ 1 Tech Module/Programming Fundamentals/3. DATA TYPES AND VARIABLES/Exercises/T13_VowelOrDigit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T13_VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = char.Parse(Console.ReadLine());
+            ch = Char.ToLower(ch);
+
+            if (Char.IsDigit(ch))
+            {
+                Console.WriteLine("digit");
+            }
+            else if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y')
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
---
+++ 1 Tech Module/Programming Fundamentals/3. DATA TYPES AND VARIABLES/Exercises/T14_IntegerToHexAndBinary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T14_IntegerToHexAndBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string numInHex = Convert.ToString(num, 16);
+            string numInBin = Convert.ToString(num, 2);
+
+            Console.WriteLine( numInHex.ToUpper() ) ;
+            Console.WriteLine(numInBin);
+
+        }
+    }
+}
