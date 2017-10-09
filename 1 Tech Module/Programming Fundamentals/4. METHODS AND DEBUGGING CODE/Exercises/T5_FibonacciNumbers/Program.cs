45cfaff40fba33933c542107519a8fd888c87fcb
GeorgiMoskov <moskov120@abv.bg>
2017-10-09 10:54:35 +0300

    T5 ex lec4

--- 1 Tech Module/Programming Fundamentals/4. METHODS AND DEBUGGING CODE/Exercises/T5_FibonacciNumbers/Program.cs
+++ 1 Tech Module/Programming Fundamentals/4. METHODS AND DEBUGGING CODE/Exercises/T5_FibonacciNumbers/Program.cs
@@ -10,9 +10,42 @@ namespace T5_FibonacciNumbers
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fib(n));
+
         }
 
-       
+        static int Fib(int n)
+        {
+            
+
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else if (n == 2)
+            {
+                return 2;
+            }
+            else
+            {
+                int numBeforeOneStep=2;
+                int numBeforeTwoSteps = 1;
+                int fibnum=0;
+
+                for (int i = 3; i <= n; i++)
+                {
+                    fibnum = numBeforeOneStep + numBeforeTwoSteps;
+                    numBeforeTwoSteps = numBeforeOneStep;
+                    numBeforeOneStep = fibnum;
+                }
+
+                return fibnum;
+
+                
+            }
+        }
+
     }
 }
