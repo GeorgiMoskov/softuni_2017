d242fc4cedff0fd32e57de1a8dac4abcbe659ffc
GeorgiMoskov <moskov120@abv.bg>
2017-10-10 22:39:57 +0300

    Т17 DEBUGGING 60/100

---
+++ 1 Tech Module/Programming Fundamentals/4. METHODS AND DEBUGGING CODE/Exercises/T16_DebugInstructionSet/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T16_DebugInstructionSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = 0;
+            while (true)
+            {
+                string opCode = Console.ReadLine();
+                string[] codeArgs = opCode.Split(' ');
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne +1;
+                            Console.WriteLine(result);
+                            break;
+                        }
+                    case "DEC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = operandOne -1;
+                            Console.WriteLine(result);
+                            break;
+                        }
+                    case "ADD":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne + operandTwo;
+                            Console.WriteLine(result);
+                            break;
+                        }
+                    case "MLA":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne * operandTwo;
+                            Console.WriteLine(result);
+                            break;
+                        }
+                    case "END":
+                        return;
+                }
+
+            }
+
+        }
+    }
+}
